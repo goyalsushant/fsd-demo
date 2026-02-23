f015e31bc54a8948fde8750d389854e2c53ef013
Sushant Goyal <goyalsushant2@gmail.com>
2026-02-23 19:29:31 +0530

    Fixed layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,11 @@ const Layout = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-800 transition-colors duration-300">
             <Navbar />
-            <main className="flex-1 p-6 flex justify-center items-center">{children}</main>
+            <main className="flex-1 p-6 flex justify-center items-center">
+                <div className="max-w-7xl mx-auto">
+                    {children}
+                </div>
+            </main>
         </div>
     )
 }
